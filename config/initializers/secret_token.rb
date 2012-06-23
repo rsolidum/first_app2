872e61fbb2a9ce875acb5daf13c81744d82f4855
rsolidum <rsolidum@gmail.com>
2012-06-23 18:45:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessonSensei::Application.config.secret_token = '5d0f0aef62f3e79da3fb3e6041acdf611575de3fbc1fda9f5d14366426a3885d9455ed59cafadb52d60346822022d9c0e0d6b4d668a3b76e05cb454b46eb8374'
